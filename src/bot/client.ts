73d49e21eabb274a3499efef296b7a5a6d4f9b70
UMCEKO <umutcevdetkocak@gmail.com>
2025-10-18 03:30:59 +0300

    feat(bot): update bot initialization and dependencies
    - Added DisconnectReason to handle connection updates more effectively.
    - Improved QR code generation logging.
    - Enhanced bot restart logic to remove auth directory on certain disconnects.
    - Updated package.json to build before running the bot.
    - Updated @whiskeysockets/baileys dependency to version 6.7.20.

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -1,21 +1,28 @@
-import { makeWASocket, useMultiFileAuthState } from '@whiskeysockets/baileys';
+import { DisconnectReason, makeWASocket, useMultiFileAuthState } from '@whiskeysockets/baileys';
 import qrcode from 'qrcode-terminal';
 import handleMessage from './handlers/messageHandler';
 import { pino } from 'pino';
+import * as fs from "fs"
+import path from 'path';
 
 export default async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState('auth');
-  const sock = makeWASocket({ auth: state, markOnlineOnConnect: false, logger: pino({ level: 'warn' }) });
+  const sock = makeWASocket({ auth: state, markOnlineOnConnect: false, logger: pino({ level: 'warn' }), version: [2, 3000, 1027934701] });
 
   sock.ev.on('creds.update', saveCreds);
 
   sock.ev.on('connection.update', (update) => {
     const { connection, qr, lastDisconnect } = update;
-    if (qr) qrcode.generate(qr, { small: true });
-    if (connection === 'close') {
-      if ((lastDisconnect?.error as any)?.output?.statusCode !== 401) startBot();
-    } else if (connection === 'open') {
-      console.log('✅ Connected');
+    if (qr) {
+      console.log("QR Created")
+      qrcode.generate(qr, { small: true })
+    };
+    if (connection === 'close' && (lastDisconnect?.error as any)?.output?.statusCode === DisconnectReason.restartRequired) {
+      return startBot()
+    }
+    else if (connection === "close") {
+      fs.rmdirSync(path.join(__dirname, "..", "..", "auth"))
+      return startBot()
     }
   });
 
